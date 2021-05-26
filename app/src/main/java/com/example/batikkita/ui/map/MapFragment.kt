ddd286ba8eb74a5f2efa7c138290c8ee28de0618
Ivan Lianto <ivanlianto59@gmail.com>
2021-05-26 19:40:48 +0700

    Map unfinished

--- app/src/main/java/com/example/batikkita/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/batikkita/ui/map/MapFragment.kt
@@ -1,40 +1,139 @@
 package com.example.batikkita.ui.map
 
+import android.annotation.SuppressLint
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.batikkita.BuildConfig.API_KEY_MAPBOX
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.example.batikkita.R
 import com.example.batikkita.databinding.FragmentMapBinding
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponent
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
 
 class MapFragment : Fragment() {
 
+    companion object {
+        private const val ICON_ID = "ICON_ID"
+    }
+
+    private lateinit var symbolManager: SymbolManager
+
     private lateinit var binding: FragmentMapBinding
     private lateinit var mapboxMap: MapboxMap
+
+    private lateinit var locationComponent: LocationComponent
+    private lateinit var mylocation: LatLng
+    private lateinit var permissionsManager: PermissionsManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Mapbox.getInstance(requireContext(), getString(R.string.mapbox_access_token))
         binding = FragmentMapBinding.inflate(inflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Mapbox.getInstance(requireContext(), getString(R.string.mapbox_access_token))
-        binding.mapView.onCreate(savedInstanceState)
-
         binding.mapView.getMapAsync { mapboxMap ->
-            mapboxMap.setStyle(Style.MAPBOX_STREETS)
             this.mapboxMap = mapboxMap
+            mapboxMap.setStyle(Style.MAPBOX_STREETS) { style ->
+                symbolManager = SymbolManager(binding.mapView, mapboxMap, style)
+                symbolManager.iconAllowOverlap = true
+                style.addImage(
+                    ICON_ID,
+                    BitmapFactory.decodeResource(resources, R.drawable.mapbox_marker_icon_default)
+                )
+                showDicodingSpace()
+                showMyHome()
+            }
+        }
+    }
+
+    private fun showDicodingSpace() {
+        val dicodingSpace = LatLng(-6.8957643, 107.6338462)
+        symbolManager.create(
+            SymbolOptions()
+                .withLatLng(LatLng(dicodingSpace.latitude, dicodingSpace.longitude))
+                .withIconImage(ICON_ID)
+                .withIconSize(1.5f)
+                .withIconOffset(arrayOf(0f, -1.5f))
+        )
+//        mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dicodingSpace, 8.0))
+    }
+
+    private fun showMyHome() {
+        val dicodingSpace = LatLng(-7.8060357, 112.0158351)
+        symbolManager.create(
+            SymbolOptions()
+                .withLatLng(LatLng(dicodingSpace.latitude, dicodingSpace.longitude))
+                .withIconImage(ICON_ID)
+                .withIconSize(1.5f)
+                .withIconOffset(arrayOf(0f, -1.5f))
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun showMyLocation(style: Style) {
+        if (PermissionsManager.areLocationPermissionsGranted(requireContext())) {
+            val customLocationComponentOptions = LocationComponentOptions.builder(requireContext())
+                .trackingGesturesManagement(true)
+                .accuracyColor(ContextCompat.getColor(requireContext(), R.color.dark_yellow))
+                .build()
+
+            val locationComponentActivationOptions =
+                LocationComponentActivationOptions.builder(requireContext(), style)
+                    .locationComponentOptions(customLocationComponentOptions)
+                    .build()
+            mapboxMap.locationComponent.apply {
+
+                activateLocationComponent(locationComponentActivationOptions)
+
+                isLocationComponentEnabled = true
+
+                cameraMode = CameraMode.TRACKING
+
+                renderMode = RenderMode.COMPASS
+            }
+        } else {
+            permissionsManager = PermissionsManager(object : PermissionsListener {
+                override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Anda harus mengizinkan location permission untuk menggunakan aplikasi ini",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onPermissionResult(granted: Boolean) {
+                    if (granted) {
+                        mapboxMap.getStyle { style ->
+                            showMyLocation(style)
+                        }
+                    } else {
+                        return
+                    }
+                }
+            })
+            permissionsManager.requestLocationPermissions(requireActivity())
         }
     }
 
@@ -67,6 +166,7 @@ class MapFragment : Fragment() {
         super.onDestroyView()
         binding.mapView.onDestroy()
     }
+
     override fun onLowMemory() {
         super.onLowMemory()
         binding.mapView.onLowMemory()
