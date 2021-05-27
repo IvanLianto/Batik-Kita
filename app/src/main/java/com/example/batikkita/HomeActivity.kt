b515b27c0fcda7bf5512d957d71888b7f1d12f22
Ivan Lianto <ivanlianto59@gmail.com>
2021-05-27 19:37:49 +0700

    Notification Activity

--- app/src/main/java/com/example/batikkita/HomeActivity.kt
+++ app/src/main/java/com/example/batikkita/HomeActivity.kt
@@ -1,22 +1,12 @@
 package com.example.batikkita
 
-import android.bluetooth.le.ScanFilter
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import androidx.navigation.plusAssign
 import androidx.navigation.ui.setupWithNavController
 import com.example.batikkita.databinding.ActivityHomeBinding
-import com.example.batikkita.navigation.KeepStateNavigator
-import com.example.batikkita.ui.cart.CartFragment
-import com.example.batikkita.ui.gift.GiftFragment
-import com.example.batikkita.ui.home.HomeFragment
-import com.example.batikkita.ui.map.MapFragment
-import com.example.batikkita.ui.notification.NotificationFragment
-import com.example.batikkita.ui.scan.ScanFragment
+import com.example.batikkita.ui.notification.NotificationActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
@@ -33,5 +23,9 @@ class HomeActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
 
         navView.setupWithNavController(navController)
+
+        binding.ivNotification.setOnClickListener {
+            startActivity(Intent(this, NotificationActivity::class.java))
+        }
     }
 }
