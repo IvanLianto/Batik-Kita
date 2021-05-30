b288f2b7834463756d58ce8641c6e3dd4e77bfae
Farlz <farrelarvino1@gmail.com>
2021-05-30 09:35:38 +0700

    DummyObject.kt

--- app/src/main/java/com/example/batikkita/utils/DummyObject.kt
+++ app/src/main/java/com/example/batikkita/utils/DummyObject.kt
@@ -4,7 +4,7 @@ import com.example.batikkita.data.source.local.entity.BatikEntity
 
 object DummyObject {
 
-    fun generateBatik(): List<BatikEntity>{
+    fun generateBatik(): List<BatikEntity> {
 
         val batik = ArrayList<BatikEntity>()
 
@@ -62,7 +62,167 @@ object DummyObject {
                 desc = "Disebut batik gentongan karena proses pembuatannya yang menggunakan gentong sebagai alat untuk merendam kain. Teknik gentong hanya dilakukan untuk satu jenis warna saja, yaitu indigo. Teknik Gentong untuk pewarna batik hanya terdapat di dua tempat yang terletak di kecamatan Tanjung Bumi, kabupaten Bangkalan. Tidak diketahui secara pasti kapan dimulainya teknik ini di Madura. Biasanya gentong diwariskan secara turun-temurun. Hasil celupan indigo yang dilakukan pada gentong hasilnya utuh, awet, dan memiliki kepekatan merata."
             )
         )
+        batik.add(
+            BatikEntity(
+                id = 6,
+                name = "Batik Ceplok",
+                image = "https://th.bing.com/th/id/OIP.nJDccL6Ie2tilSELNzUBWQAAAA?w=115&h=180&c=7&o=5&pid=1.7",
+                origin = "Yogyakarta",
+                island = "Jawa",
+                desc = "Batik ceplok atau ceplokan merupakan jenis batik yang memiliki pola atau motif dengan bentuk dasar geometri, seperti persegi, oval maupun bintang yang disusun melingkar sehingga menyerupai sekuntum bunga dengan pola simetris. Bentuk tersebut terinsirasi dari buah kawung atau buah aren yang dibelah empat."
+            )
+        )
+        batik.add(
+            BatikEntity(
+                id = 7,
+                name = "Batik Ciamis",
+                image = "https://th.bing.com/th/id/OIP.bSirKRst-QdyTSlagsyOkAHaE8?w=269&h=180&c=7&o=5&pid=1.7",
+                origin = "Jawa Barat",
+                island = "Jawa",
+                desc = "Batik Ciamis memiliki karakter dan corak batik yang berbeda dengan batik Garut dan Tasik. Batik ciamisan tampil sederhana tapi penuh wibawa. Kesederhaan ini tak lepas dari sejarah keberadaannya yang banyak dipengaruhi daerah lain, seperti ragam hias pesisiran dari Indramayu dan Cirebon. Selain itu, pengaruh batik nonpesisiran, seperti dari Solo dan Yogyakarta, tak kalah dominan."
+            )
+        )
+        batik.add(
+            BatikEntity(
+                id = 8,
+                name = "Batik Garutan",
+                image = "https://th.bing.com/th/id/OIP.KlwLYLv6ccVLK1Feza4-iwHaFj?w=210&h=180&c=7&o=5&pid=1.7",
+                origin = "Garut, Jawa Barat",
+                island = "Jawa",
+                desc = "Motif Batik Garutan selalu identik dengan motif-motif batik yang berasal dari alam pegunungan, flora dan fauna yang memiliki kaitan erat dengan keseharian masyarakat Garut terutama sebelum masa kemerdekaan. Motif Batik Garutan terinspirasi dari kondisi alam dan lingkungan masyarakat Garut sendiri, dari mulai motif batik Bulu Hayam hingga motif Lereng Kangkung."
+            )
+        )
+        batik.add(
+            BatikEntity(
+                id = 9,
+                name = "Batik Kawung",
+                image = "https://th.bing.com/th/id/OIP.nLVR973i4AY8rMoHIb2GhAHaJ4?w=126&h=180&c=7&o=5&pid=1.7",
+                origin = "Jawa",
+                island = "Jawa",
+                desc = "Batik Kawung adalah motif batik yang bentuknya berupa bulatan mirip buah kawung (sejenis kelapa atau kadang juga dianggap sebagai aren atau kolang-kaling) yang ditata rapi secara geometris. Kadang, motif ini juga ditafsirkan sebagai gambar bunga lotus (teratai) dengan empat lembar mahkota bunga yang merekah. Lotus adalah bunga yang melambangkan umur panjang dan kesucian."
+            )
+        )
+        batik.add(
+            BatikEntity(
+                id = 10,
+                name = "Batik Keraton",
+                image = "https://th.bing.com/th/id/OIP.kw-895hpAhckMHz2K5PMxQHaLH?w=115&h=180&c=7&o=5&pid=1.7",
+                origin = "Yogyakarta",
+                island = "Jawa",
+                desc = "Batik keraton (dikenal juga dengan istilah batik larangan atau batik vorstenlanden) adalah batik yang berkembang dalam lingkungan keraton, baik Yogyakarta maupun Surakarta. Batik keraton merupakan awal mula dari semua jenis batik yang berkembang di Indonesia. Motifnya mengandung beragam makna filosofi hidup yang banyak terilhami dari kebudayan Hindu-Jawa. Batik-batik ini dibuat oleh para putri keraton dan juga pembatik-pembatik ahli yang hidup di lingkungan keraton. Pada dasarnya motifnya terlarang untuk digunakan oleh orang “biasa” seperti motif Batik Parang Barong, Batik Parang Rusak termasuk Batik Udan Liris, dan beberapa motif lainnya."
+            )
+        )
 
-        return batik
+        batik.add(
+            BatikEntity(
+                id = 11,
+                name = "Batik Lasem",
+                image = "https://th.bing.com/th/id/OIP.EioGZ1j5nehrrt66gbAcAAHaFj?w=213&h=180&c=7&o=5&pid=1.7",
+                origin = "Jawa",
+                island = "Jawa",
+                desc = "Batik lasem adalah batik nusantara hasil perpaduan dari dua budaya yakni Tionghoa dan Jawa. Secara umum, batik ini memiliki dua motif utama, yakni motif Tionghoa dengan gambar burung hong, naga, ayam hutan, dan sebagainya, sedangkan motif non-Tionghoa bergambar sekar jagad, kendoro kendiri, kricak, grinsing, dan lainnya. Batik lasem juga dikenal sebagi batik tiga negeri karena memiliki tiga kali proses pewarnaan di tiga tempat berbeda. Warna merah di Lasem, biru di Pekalongan, dan Cokelat di Solo."
+            )
+        )
+
+        batik.add(
+            BatikEntity(
+                id = 12,
+                name = "Batik Megamendung",
+                image = "https://th.bing.com/th/id/OIP.rCcIsuRjhanzIjw9lg3uoQHaHa?w=198&h=198&c=7&o=5&pid=1.7",
+                origin = "Cirebon, Jawa Barat",
+                island = "Jawa",
+                desc = "Motif batik Megamendung merupakan karya seni batik yang identik dan bahkan menjadi ikon batik daerah Cirebon dan daerah Indonesia lainnya. Motif batik ini mempunyai kekhasan yang tidak ditemui di daerah penghasil batik lain. Bahkan karena hanya ada di Cirebon dan merupakan mahakarya, Departemen Kebudayaan dan Pariwisata akan mendaftarkan motif megamendung ke UNESCO untuk mendapatkan pengakuan sebagai salah satu warisan dunia."
+            )
+        )
+
+        batik.add(
+            BatikEntity(
+                id = 13,
+                name = "Batik Parang",
+                image = "https://th.bing.com/th/id/OIP._kf_2DziiUGc-r5J2o39fgHaD4?w=339&h=181&c=7&o=5&pid=1.7",
+                origin = "Jawa",
+                island = "Jawa",
+                desc = "Batik Parang merupakan salah satu motif batik yang paling tua di Indonesia. Parang berasal dari kata Pereng yang berarti lereng. Perengan menggambarkan sebuah garis menurun dari tinggi ke rendah secara diagonal. Susunan motif S jalin-menjalin tidak terputus melambangkan kesinambungan."
+            )
+        )
+
+        batik.add(
+            BatikEntity(
+                id = 14,
+                name = "Batik Pekalongan",
+                image = "https://th.bing.com/th/id/OIP.fiEyAQq72r73-Yhwh4rP4QAAAA?w=142&h=190&c=7&o=5&pid=1.7",
+                origin = "Pekalongan, Jawa Tengah",
+                island = "Jawa",
+                desc = "Batik Pekalongan termasuk batik pesisiran. Dari sisi motif, sebenarnya batik Pekalongan mirip dengan batik Solo maupun Yogyakarta. Keunggulan batik Pekalongan terletak pada penggunaan warna. Sehelai kain batik bisa menggunakan delapan warna sehingga terlihat lebih indah dan menarik dibanding batik-batik dari daerah lain"
+            )
+        )
+
+        batik.add(
+            BatikEntity(
+                id = 15,
+                name = "Batik Priangan",
+                image = "https://th.bing.com/th/id/OIP.ByRn0OmN0M3R1UzJr2hmTAAAAA?w=187&h=125&c=7&o=5&pid=1.7",
+                origin = "Jawa Barat",
+                island = "Jawa",
+                desc = "Batik yang berasal dari Jawa barat ini biasa disebut dengan batik Priangan, istilah ini digunakan untuk memberikan identitas pada berbagai batik yang dihasilkan dan berlangsung di Priangan. Secara keseluruhan, kesan yang didapat saat melihat selembar batik priangan adalah kesan cantik-molek, bahkan sedikit genit, yang mungkin selaras dengan citra orang Sunda."
+            )
+        )
+
+        batik.add(
+            BatikEntity(
+                id = 16,
+                name = "Batik Sekar",
+                image = "https://th.bing.com/th/id/OIP._jdwFac0FHn7EUxlC3YpnwHaD4?w=310&h=180&c=7&o=5&pid=1.7",
+                origin = "Jawa",
+                island = "Jawa",
+                desc = "Batik Sekar Jagad adalah salah satu motif batik khas Indonesia. Motif ini mengandung makna kecantikan dan keindahan sehingga orang lain yang melihat akan terpesona. Ada pula yang beranggapan bahwa motif Sekar Jagad sebenarnya berasal dari kata \"kar jagad\" yang diambil dari bahasa Jawa (Kar=peta; Jagad=dunia), sehingga motif ini juga melambangkan keragaman di seluruh dunia."
+            )
+        )
+
+        batik.add(
+            BatikEntity(
+                id = 17,
+                name = "Batik Sidoluhur",
+                image = "https://th.bing.com/th/id/OIP.qGMHD6toLUR8tnrG_v8quQHaEN?w=259&h=183&c=7&o=5&pid=1.7",
+                origin = "Jawa",
+                island = "Jawa",
+                desc = "Batik Sidoluhur adalah salah satu motif batik pedalaman yang cukup populer. Sesuai dengan namanya, motif ini bermakna harapan untuk mencapai kedudukan yang tinggi dan dapat menjadi panutan masyarakat (keluhuran). Motif-motif batik berawalan sida (dibaca sido) merupakan golongan motif yang banyak dibuat para pembatik. Kata “sida” dalam bahasa Jawa sendiri berarti jadi/menjadi/terlaksana. Dengan demikian, motif-motif berawalan “sida” mengandung harapan agar apa yang diinginkan bisa tercapai."
+            )
+        )
+
+        batik.add(
+            BatikEntity(
+                id = 18,
+                name = "Batik Sidomukti",
+                image = "https://th.bing.com/th/id/OIP.xS3cJXJ-lmMhxBG8vUiavQHaHa?w=166&h=180&c=7&o=5&pid=1.7",
+                origin = "Jawa",
+                island = "Jawa",
+                desc = "batik Sidomukti meruapakan motif batik yang biasanya terbuat dari zat pewarna soga alam. Biasanya digunakan sebagai kain dalam upacara perkawinan. Unsur motif yang tekandung didalamnya adalah gurda. Motif-motif berawalan sida (dibaca sido) merupakan golongan motif yang banyak dibuat para pembatik. Kata “sida” sendiri berarti jadi/menjadi/terlaksana. Dengan demikian, motif-motif berawalan “sida” mengandung harapan agar apa yang diinginkan bias tercapai. Salah satunya adalah sida mukti, yang mengandung harapan untuk mencapai kebahagiaan lahir dan batin."
+            )
+        )
+
+        batik.add(
+            BatikEntity(
+                id = 19,
+                name = "Batik Sogan",
+                image = "https://th.bing.com/th/id/OIP.jkZwvj9-ZouD8m7VBtLLlwHaJ4?w=124&h=180&c=7&o=5&pid=1.7",
+                origin = "Yogyakarta",
+                island = "Jawa",
+                desc = "Batik Sogan merupakan salah satu jenis batik bernuansa klasik variasi warna didominasi warna coklat. Dinamakan batik sogan karena pada awal mulanya, proses pewarnaan batik ini menggunakan pewarna alami yang diambil dari batang kayu pohon soga tingi. Batik Sogan identik dengan daerah keraton Jawa seperti Yogyakarta dan Solo, motifnya pun biasanya mengikuti pakem motif-motif klasik keraton."
+            )
+        )
+
+        batik.add(
+            BatikEntity(
+                id = 20,
+                name = "Batik Tambal",
+                image = "https://th.bing.com/th/id/OIP.Fa8OjgvHAmgM6BXKsb_a-wAAAA?w=132&h=180&c=7&o=5&pid=1.7",
+                origin = "Yogyakarta",
+                island = "Jawa",
+                desc = "Motif batik tambal memiliki arti tambal bermakna menambal atau memperbaiki hal-hal yang rusak. Dalam perjalanan hidupnya, manusia harus memperbaiki diri menuju kehidupan yang lebih baik, lahir maupun batin. Dahulu, kain batik bermotif tambal dipercaya bisa membantu kesembuhan orang yang sakit. Caranya adalah dengan menyelimuti orang sakit tersebut dengan kain motif tambal."
+            )
+        )
+
+            return batik
     }
 }
